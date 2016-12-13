21889861bf91a2ac3cb6ab80b04811f36d60b35c
slanto <slanto@slanto.com>
2016-12-13 13:53:47 +0100

    Adding firt fake methods for getting EasterSunday and NewYear with unit tests

--- tests/example.js
+++ tests/example.js
@@ -1,5 +1,17 @@
-describe('example test', function() {
+describe('first sample test', function() {
   it('should be true', function() {
     expect('bar').toBe('bar');
   });
+});
+
+describe('PolishHolidays', function(){
+    it('should be defined', function() {
+        expect(polishHolidays).toBeDefined();
+    });
+    it('polishHolidays.easterSunday() should be defined', function() {
+        expect(polishHolidays.easterSunday(2017)).toBeDefined();
+    });
+    it('polishHolidays.easterSunday(2017) should return easter date', function() {
+        expect(polishHolidays.easterSunday(2017)).toEqual({ day: 16, month: 4, year: 2017, description: 'Easter' });
+    });    
 });
