209896b4944433eba2f370a1764bf00f44154aa4
slanto <slanto@slanto.com>
2016-12-13 15:48:18 +0100

    Implementing function for getting Easter date based on Meeusa/Jonesa/Butchera algorithm

--- tests/example.js
+++ tests/example.js
@@ -14,4 +14,10 @@ describe('PolishHolidays', function(){
     it('polishHolidays.easterSunday(2017) should return easter date', function() {
         expect(polishHolidays.easterSunday(2017)).toEqual({ day: 16, month: 4, year: 2017, description: 'Easter' });
     });    
+    it('polishHolidays.easterSunday(2016) should return easter date', function() {
+        expect(polishHolidays.easterSunday(2016)).toEqual({ day: 27, month: 3, year: 2016, description: 'Easter' });
+    }); 
+    // it('polishHolidays.easterSunday(-1) should return invalid year exception', function() {
+    //     expect(polishHolidays.easterSunday(-1)).toThrow();
+    // });
 });
