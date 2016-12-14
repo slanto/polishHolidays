9464ebb26154fd4f47a4c2a884b78d5caa8a0a7d
slanto <slawek.antos@outlook.com>
2016-12-14 14:26:05 +0100

    Using javascript Date type

--- tests/example.js
+++ tests/example.js
@@ -12,12 +12,18 @@ describe('PolishHolidays', function(){
         expect(polishHolidays.easterSunday(2017)).toBeDefined();
     });
     it('polishHolidays.easterSunday(2017) should return easter date', function() {
-        expect(polishHolidays.easterSunday(2017)).toEqual({ day: 16, month: 4, year: 2017, description: 'Easter' });
+        expect(polishHolidays.easterSunday(2017)).toEqual(new Date('Sun Apr 16 2017'));
     });    
     it('polishHolidays.easterSunday(2016) should return easter date', function() {
-        expect(polishHolidays.easterSunday(2016)).toEqual({ day: 27, month: 3, year: 2016, description: 'Easter' });
+        expect(polishHolidays.easterSunday(2016)).toEqual(new Date('Sun Mar 27 2016'));
     }); 
     // it('polishHolidays.easterSunday(-1) should return invalid year exception', function() {
     //     expect(polishHolidays.easterSunday(-1)).toThrow();
     // });
+    it('polishHolidays.all(2017) should be defined', function() {
+        expect(polishHolidays.all(2017)).toBeDefined();
+    });
+    it('polishHolidays.all(2017) should return all holidays', function() {
+        //expect(polishHolidays.all(2017)).toEqual({ 0: [{ date: new Date('Sun Jan 01 2017'), description: 'New Year\'s Day'}], 3: [{date: new Date('Sun Apr 16 2017'), description: 'Easter Day'}]});
+    });
 });
